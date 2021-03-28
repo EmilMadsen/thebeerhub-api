d75ecb2245ae193af66c65ee8fbbe9992b12d7df
Emil Madsen <emilmadsen@gmail.com>
2021-03-28 01:30:20 +0100

    added more fields to brew dto

--- app/main/model/dto.py
+++ app/main/model/dto.py
@@ -14,6 +14,15 @@ class UserDto:
 class BrewDto:
     api = Namespace('brew', description='brew related operations')
     brew = api.model('brew', {
+        'id': fields.String(required=True, description='brew identifier'),
         'brew_name': fields.String(required=True, description='brew name'),
-        'brew_type': fields.String(required=True, description='brew type')
+        'brew_type': fields.String(required=True, description='brew type'),
+        'datetime': fields.DateTime(description='brew date'),
+        'brewsters': fields.String(description='who brewed the brew'),
+        'location': fields.String(description='where was it brewed'),
+        'recipe': fields.String(description='link to recipe'),
+        'target_start_gravity': fields.Integer(description='target start gravity'),
+        'actual_start_gravity': fields.Integer(description='actual start gravity'),
+        'target_end_gravity': fields.Integer(description='target end gravity'),
+        'actual_end_gravity': fields.Integer(description='actual end gravity'),
     })
